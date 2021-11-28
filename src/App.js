4ed282d183be1d005c0bdcb6d19186f203c45842
Piotr Olejarz <olejarz-piotr@o2.pl>
2021-11-29 00:48:15 +0100

    creating view of searched colors with weight

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
 
     try {
       let colors = new Values(color).all(10);
+      setList(colors);
     } catch (error) {
       setError(true);
     }
@@ -36,7 +37,9 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        <h4>list goes here</h4>
+        {list.map((color, index) => {
+          return <SingleColor key={index} {...color} index={index} />
+        })}
       </section>
     </>
   );
