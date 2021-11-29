d0d5cce8776904ba1ed4636977f273eb4579e1d6
Piotr Olejarz <olejarz-piotr@o2.pl>
2021-11-29 01:00:00 +0100

    edting view of colors, adding to clipboard option

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,14 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
 
-const SingleColor = ({ rgb, weight, index,hexColor }) => {
-  const [alert, setAlert] = useState(false)
-  const bcg = rgb.join(',');
-const hex = rgbToHex(...rgb)
-  return <article className={`color`} style={{ backgroundColor: `rgb(${bcg})` }}>
-    <p className="percent-value">{weight}%</p>
-    <p className="color-value">{hex}</p>
-  </article>
-}
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  return (
+    <article
+      className={`color ${index > 10 && "color-light "}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexColor)
+      }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexColor}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
+    </article>
+  );
+};
 
-export default SingleColor
+export default SingleColor;
